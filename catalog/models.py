1a9196d3fdbb41ab3748ba190fa24c16755714a5
avdeevaa <tess-avdeeva@seznam.cz>
2023-11-23 13:37:39 +0100

    HW 19.2

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.CharField(max_length=100, verbose_name='описание')
+    image = models.ImageField(verbose_name='изображение (превью)', null=True, blank=True)
+    category = models.CharField(max_length=100, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена за штуку')
+    creation_date = models.DateField(verbose_name='дата создания')
+    modification_date = models.DateField(verbose_name='дата последнего изменения')
+
+
+    def __str__(self):
+        return f"{self.name}, {self.category}, {self.price}"
+
+    class Meta:
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
+        ordering = ('name',)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.CharField(max_length=100, verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name}: {self.description}'
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+        ordering = ('name',)
--- config/settings.py
+++ config/settings.py
@@ -77,11 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_db', # Название БД
+        'USER': 'postgres', # Пользователь для подключения
+        'PASSWORD': '1234', # Пароль для этого пользователя
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
