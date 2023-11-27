a073281db640b9ab6c2b9b782ca36e3f1b655ab2
avdeevaa <tess-avdeeva@seznam.cz>
2023-11-27 09:37:12 +0100

    HW 20.2 omg it works but...

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from catalog.views import main_page, contact_page
+from catalog.views import main_page, contact_page, items_page
 
 urlpatterns = [
-    path('', main_page, name='main_page'),
+    path('main/', main_page, name='main_page'),
     path('contacts/', contact_page, name='contact_page'),
+    path('items/', items_page, name='items_page'),
+
 ]
+
--- catalog/views.py
+++ catalog/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from catalog.models import Category
+
+# def main_page(request):
+#     return render(request, 'design/main_page.html')
+
 
 def main_page(request):
-    return render(request, 'design/main_page.html')
+    categories_list = Category.objects.all()
+    context = {
+        'object_list': categories_list
+    }
+    return render(request, 'design/main_page.html', context)
 
 
 def contact_page(request):
@@ -14,3 +23,8 @@ def contact_page(request):
         message = request.POST.get('message')
         print(f"Форма обрабатывает {name}, {phone}, {message}")
     return render(request, 'design/contact_page.html')
+
+
+def items_page(request):
+    return render(request, 'design/items_page.html')
+
